0ab6eadcd8dd8b8c31df0b569f34c88e2e78c04e
Gonzalo Viglioni <122325178+gonza87@users.noreply.github.com>
2024-08-30 13:24:43 -0300

    estado

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,11 +1,17 @@
 import { useState } from "react";
 
 function GifExpertApp() {
-  const [categories, setcategories] = useState(["one", "two"]);
+  const [categories, setCategories] = useState(["one", "two"]);
+
+  const onAddcategory = () => {
+    setCategories([...categories, "test"]);
+    console.log(categories);
+  };
 
   return (
     <>
       <h1>Gif</h1>
+      <button onClick={onAddcategory}>Agregar categoria</button>
       <ol>
         {categories.map((category) => {
           return <li key={category}>{category}</li>;
