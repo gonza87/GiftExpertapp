82eb45dd349a133065d3a0b576cdc5ba21afc766
Gonzalo Viglioni <122325178+gonza87@users.noreply.github.com>
2024-09-03 15:09:43 -0300

    getGifs

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,17 +1,5 @@
 import React from "react";
-
-const getGifs = async (category) => {
-  const url = `https://api.giphy.com/v1/gifs/search?api_key=kYQMrvSee6kqr2cGrSGr6WXK7652mS08&q=${category}&limit=20`;
-  const resp = await fetch(url);
-  const { data } = await resp.json();
-  const gifs = data.map((img) => ({
-    id: img.id,
-    title: img.title,
-    url: img.images.downsized_medium.url,
-  }));
-  console.log(gifs);
-  return gifs;
-};
+import { getGifs } from "../helpers/getGifs";
 
 function GifGrid({ category }) {
   getGifs(category);
