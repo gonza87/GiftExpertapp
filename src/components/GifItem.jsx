e20971996274d88326a50179ed66f00dab2cd27e
Gonzalo Viglioni <122325178+gonza87@users.noreply.github.com>
2024-09-04 12:50:40 -0300

    titleimage

--- src/components/GifItem.jsx
+++ src/components/GifItem.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
-function GifItem() {
-  return <div>GifItem</div>;
+function GifItem({ image }) {
+  console.log(image.title);
+  return (
+    <div>
+      <p>{image.title}</p>
+    </div>
+  );
 }
 
 export default GifItem;
