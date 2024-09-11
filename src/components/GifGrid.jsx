447792669c26d0a18089797e2385e4d66a63e5b6
Gonzalo Viglioni <122325178+gonza87@users.noreply.github.com>
2024-09-11 11:24:26 -0300

    Revert "hook"
    This reverts commit a85248fbf8bfb04d5fbd84635434334f1ac5d304.

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,9 +1,22 @@
 import React from "react";
+import { useEffect, useState } from "react";
+import { getGifs } from "../helpers/getGifs";
 import GifItem from "./GifItem";
-import { useFetchGifs } from "../hooks/useFetchGifs";
 
 function GifGrid({ category }) {
-  const { images, loading } = useFetchGifs(category);
+  const [images, setImages] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const getImages = async () => {
+    const newImages = await getGifs(category);
+    setImages(newImages);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    getImages();
+  }, []);
+
   return (
     <>
       <h3>{category}</h3>
